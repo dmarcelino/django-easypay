5ccc2938826d8301cd03c9c018a242396fc1f126
Dario Marcelino <dario@appscot.com>
2019-02-12 19:46:30 +0000

    move version to init, bump it to 0.2.0

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
+#!/usr/bin/env python
+import os
+import re
 import setuptools
+
+
+def get_version(*file_paths):
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Please assure that the package version is defined as "__version__ = x.x.x" in ' + filename)
+
+version = get_version("easypay", "__init__.py")
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="django-easypay",
-    version="0.1.1",
+    version=version,
     author="Dario Marcelino",
     author_email="dario@appscot.com",
     install_requires=[
